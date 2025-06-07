8488d9197a19ef95e43e95209c44bf618d670ae4
kai <kaishun.teoh@gmail.com>
2025-06-07 16:39:02 +0800

    lol

--- utils/graph.py
+++ utils/graph.py
@@ -156,17 +156,7 @@ class LocalGraph:
     
     # aux functions
     # initialise point clouds into nodes
-    # now i know how to add geometric features to the objects
-    # basically what we do is kinda the same as how we link agent nodes (have a separate function for this)
-    # what we will do is first add in the dict a list of (tl,tr,br,bl,center) and use it to do clustering algorithm (K means)
-        # a fun thing about K means is that the funciton used to claculate k means and distance can be moddded to mobius operations!
-    # then using the segmented set of nodes, we further reduce them 
-        # first use corners to define boundary (what do we do as center? maybe can encode gemoetry | center, graph) s.t given center, we can predict geometry of object
-        # then create a circular graph
-        # then topologically sort the nodes with linear operations (|p1 - center| = distance (metric to used to topologically sort them {can even think of encodding vectors}) )
-            # mobius operation can be used here too! 
-        # then find nearest nodes to each edge and connect them 
-        # graphs then becomes sparse
+
     def _init_nodes(self, agent_pos, point_clouds):
         
         # init agent nodes
@@ -207,12 +197,27 @@ class LocalGraph:
         for (i,node) in enumerate(self.nodes):
             self.node_idx_dict[node] = i
     
+
     # according to image, it seems to be a fully connected graph
     # edge[i,j] means i connected to j
     # for agent -> edibles edge points from agent to edibles
     # for agent -> goals edge points from agent to goals
     # for agent -> obstacles edge points from obstacles to agent
     # for edible1 to edible2, edge points bothways
+
+    # now i know how to add geometric features to the objects
+    # basically what we do is kinda the same as how we link agent nodes (have a separate function for this)
+    # what we will do is first add in the dict a list of (tl,tr,br,bl,center) and use it to do clustering algorithm (K means)
+        # a fun thing about K means is that the funciton used to claculate k means and distance can be moddded to mobius operations!
+    # then using the segmented set of nodes, we further reduce them 
+        # first use corners to define boundary (what do we do as center? maybe can encode gemoetry | center, graph) s.t given center, we can predict geometry of object
+        # then create a circular graph
+        # then topologically sort the nodes with linear operations (|p1 - center| = distance (metric to used to topologically sort them {can even think of encodding vectors}) )
+            # mobius operation can be used here too! 
+        # then find nearest nodes to each edge and connect them 
+        # graphs then becomes sparse
+    # note thaat this graph can be parsed into a transformer and encoded to become a feature! (poinet++ and occupancy map might help)
+        # if anything we are removing occupancy net, but pointnet++ can still be used as a feature metric
     def _init_edges(self):
         edges = []
 
@@ -238,6 +243,15 @@ class LocalGraph:
                     )
 
         self.edges = edges
+
+
+    # return a list of edge object
+    def _connect_set_of_nodes(self, corner_node, center_node, inner_node):
+        pass
+
+    # takes in a node and a dict of (tl,tr,br,bl) boundary and returns which boundary it retruns to
+    def _cluster_node(self,boundaries ,node):
+        pass
 
 
     def draw_graph(self):
