e847ac8174ac6a64c99905ae2dc9aeb46d0b51d9
kai <kaishun.teoh@gmail.com>
2025-08-11 13:48:15 +0100

    tweaks to test large actions

--- configs.py
+++ configs.py
@@ -6,7 +6,7 @@ from tasks2d import LousyPacmanPseudoMaxRot as  PSEUDO_MAX_ROTATION
 
 
 _type = 'vanilla' # | "control" | "PC_EMB" | "PC_EMB_hyperbolic"
-version = '4s'
+version = '4'
 geo_version = '2'
 
 SAMPLING_RATE = 1
